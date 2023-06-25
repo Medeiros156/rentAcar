c0639ff10cb620c536368acf6748bbeffe2ef328
Pedro Medeiros <pedromedeiros156@hotmail.com>
2023-06-25 17:32:24 -0300

    final

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route index path="/" element={<Home />} />
+        <Route index path="/rentAcar" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="models" element={<Models />} />
         <Route path="testimonials" element={<TestimonialsPage />} />
